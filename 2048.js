d420e80d5a1aabcac74d7e33b7aa9f295760582e
ssikme <858585801@qq.com>
2019-04-03 19:43:55 +0800

    修复bug和完善gameover

--- 2048.js
+++ 2048.js
@@ -1,56 +1,58 @@
 window.onload = function () {
     randomGeneration();
-}
-
-var ans=0;
-var mark=document.getElementById("mark");
-mark.innerHTML="<b id='fenshu'>分数</b><br><b id='ans'>"+ans+"</b>";
-var rcs = document.getElementsByClassName("rc");
-var board = new Array(4);
-for (var i = 0; i < 4; i++)
+};
+let tryagain=document.getElementById("try-again");
+let ans=0;
+let mark=document.getElementById("ans");
+mark.innerHTML=ans;
+let rcs = document.getElementsByClassName("rc");
+let board = new Array(4);
+for (let i = 0; i < 4; i++)
     board[i] = new Array(4);
-for (var i = 0; i < 4; i++)
-    for (var j = 0; j < 4; j++) board[i][j] = 0;
+for (let i = 0; i < 4; i++)
+    for (let j = 0; j < 4; j++) board[i][j] = 0;
 
 function init() {
-    for (var i = 0; i < 4; i++)
-        for (var j = 0; j < 4; j++) board[i][j] = 0;
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 4; j++) board[i][j] = 0;
     ans=0;
 }
 
 function randomGeneration() {
-    var sign = new Array();
-    var lose = true;
-    var k = 0;
-    for (var i = 0; i < 4; i++) {
-        for (var j = 0; j < 4; j++) {
+    let sign = [ ];
+    let lose = true;
+    let k = 0;
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
             if (board[i][j] === 0) {
-                lose = false;
+                lose = false;//有空格
                 sign[k] = 4 * i + j;
                 k++;
             }
         }
     }
-    if (lose === true) {
-        lose_f();
-    } else {
-        var cnt = Math.ceil(Math.random() * 2);
-        for (var i = 0; i < cnt; i++) {//生成一个或两个
-            var index = sign[Math.ceil(Math.random() * (k + 1)) - 1];//取得随机生成的棋块编号
-            var str = "<b>2</b>";
+    if(lose===false){//有空格，随机生成
+        let cnt = Math.ceil(Math.random() * 2);
+        for (let i = 0; i < cnt&&i<k; i++) {//生成一个或两个,当空格只剩下1(k=1)只生成一个
+            let index = sign[Math.floor(Math.random()*k)];//取得随机生成的棋块编号
+            let str = "<b>2</b>";
             board[Math.floor(index / 4)][index % 4] = 2;
-            rcs[(index % 4) * 4 + Math.floor(index / 4)].className="rc color-2";
+            rcs[(index % 4) * 4 + Math.floor(index / 4)].className = "rc color-2";
             rcs[(index % 4) * 4 + Math.floor(index / 4)].innerHTML = str;
             renderBoard();
         }
     }
+    else {//满格
+        if(is_Lose()===true) lose_f();//满格且为输
+    }
 }
 
+
 function toRight() {
-    for (var i = 0; i < 4; i++) {
-        var nums = new Array(),//记录非空的数
+    for (let i = 0; i < 4; i++) {
+        let nums = [ ],//记录非空的数
             k = 0;
-        for (var j = 0; j < 4; j++)
+        for (let j = 0; j < 4; j++)
             if (board[i][j] > 0) {
                 nums[k] = board[i][j];
                 k++;
@@ -73,16 +75,16 @@ function toRight() {
             } else if (nums[2] === nums[1]) board[i][3] = nums[3], board[i][2] = 2 * nums[1], board[i][1] = nums[0], board[i][0] = 0,ans+=2 * nums[1];
             else if (nums[1] === nums[0]) board[i][3] = nums[3], board[i][2] = nums[2], board[i][1] = 2 * nums[0], board[i][0] = 0;
             else
-                for (var p = 0; p < 4; p++) board[i][p] = nums[p];
+                for (let p = 0; p < 4; p++) board[i][p] = nums[p];
         }
     }
 }
 
 function toLeft() {
-    for (var i = 0; i < 4; i++) {
-        var nums = new Array(),//记录非空的数
+    for (let i = 0; i < 4; i++) {
+        let nums = [ ],//记录非空的数
             k = 0;
-        for (var j = 0; j < 4; j++)
+        for (let j = 0; j < 4; j++)
             if (board[i][j] > 0) {
                 nums[k] = board[i][j];
                 k++;
@@ -105,15 +107,15 @@ function toLeft() {
             } else if (nums[1] === nums[2]) board[i][0] = nums[0], board[i][1] = 2 * nums[1], board[i][2] = nums[3], board[i][3] = 0,ans+=2 * nums[1];
             else if (nums[2] === nums[3]) board[i][0] = nums[0], board[i][1] = nums[1], board[i][2] = 2 * nums[3], board[i][3] = 0,ans+=2 * nums[3];
             else
-                for (var p = 0; p < 4; p++) board[i][p] = nums[p];
+                for (let p = 0; p < 4; p++) board[i][p] = nums[p];
         }
     }
 }
 
 function toDown() {
-    for (var j = 0; j < 4; j++) {
-        var nums = new Array(), k = 0;
-        for (var i = 0; i < 4; i++)
+    for (let j = 0; j < 4; j++) {
+        let nums = [ ], k = 0;
+        for (let i = 0; i < 4; i++)
             if (board[i][j] > 0) {
                 nums[k] = board[i][j];
                 k++;
@@ -133,15 +135,15 @@ function toDown() {
             } else if (nums[2] === nums[1]) board[3][j] = nums[3], board[2][j] = 2 * nums[2], board[1][j] = nums[1], board[0][j] = 0,ans+=2 * nums[2];
             else if (nums[1] === nums[0]) board[3][j] = nums[3], board[2][j] = nums[2], board[1][j] = 2 * nums[1], board[0][j] = 0,ans+=2 * nums[1];
             else
-                for (var p = 0; p < 4; p++) board[p][j] = nums[p];
+                for (let p = 0; p < 4; p++) board[p][j] = nums[p];
         }
     }
 }
 
 function toUp() {
-    for (var j = 0; j < 4; j++) {
-        var nums = new Array(), k = 0;
-        for (var i = 0; i < 4; i++)
+    for (let j = 0; j < 4; j++) {
+        let nums = [ ], k = 0;
+        for (let i = 0; i < 4; i++)
             if (board[i][j] > 0) {
                 nums[k] = board[i][j];
                 k++;
@@ -159,63 +161,77 @@ function toUp() {
                 if (nums[2] === nums[3]) board[0][j] = 2 * nums[0], board[1][j] = 2 * nums[2], board[2][j] = 0, board[3][j] = 0,ans+=2 * nums[0],ans+=2 * nums[2];
                 else board[0][j] = 2 * nums[0], board[1][j] = nums[2], board[2][j] = nums[3], board[3][j] = 0,ans+=2 * nums[0];
             } else if (nums[1] === nums[2]) board[0][j] = nums[0], board[1][j] = 2 * nums[2], board[2][j] = nums[3], board[3][j] = 0,ans+=2 * nums[2];
-            else if (nums[2] === nums[3]) board[0][j] = nums[0], board[1][j] = 2 * nums[1], board[2][j] = 2 * nums[2], board[3][j] = 0,ans+=2 * nums[1];
+            else if (nums[2] === nums[3]) board[0][j] = nums[0], board[1][j] = nums[1], board[2][j] = 2 * nums[2], board[3][j] = 0,ans+=2 * nums[1];
             else
-                for (var p = 0; p < 4; p++) board[p][j] = nums[p];
+                for (let p = 0; p < 4; p++) board[p][j] = nums[p];
         }
     }
 }
 
 function renderBoard() {
-    for (var i = 0; i < 4; i++)
-        for (var j = 0; j < 4; j++) {
-            if(board[i][j]>0) {
-                str="<b>"+board[i][j]+"</b>";
-                if (board[i][j] === 2)rcs[4 * j + i].className="rc color-2";
-                else if (board[i][j] === 4) rcs[4 * j + i].className="rc color-4";
-                else if (board[i][j] === 8) rcs[4 * j + i].className="rc color-8";
-                else if (board[i][j] === 16) rcs[4 * j + i].className="rc  color-16";
-                else if (board[i][j] === 32) rcs[4 * j + i].className="rc  color-32";
-                else if (board[i][j] === 64) rcs[4 * j + i].className="rc  color-64";
-                else if (board[i][j] === 128) rcs[4 * j + i].className="rc  color-128";
-                else if (board[i][j] === 256)rcs[4 * j + i].className="rc color-256";
-                else if (board[i][j] === 512) rcs[4 * j + i].className="rc  color-512";
-                else if (board[i][j] === 1024) rcs[4 * j + i].className="rc four color-1024";
-                else  rcs[4 * j + i].className+="rc big color-2048";
+    for (let i = 0; i < 4; i++)
+        for (let j = 0; j < 4; j++) {
+               let str = "<b>" + board[i][j] + "</b>";
+                let newClassName = null;
+                if(board[i][j]===0){
+                    str="";
+                    newClassName="rc big color-0";
+                }
+                else
+                    newClassName="rc color-"+board[i][j];
+                rcs[4 * j + i].className = newClassName;
                 rcs[4 * j + i].innerHTML = str;
-            }
-            else {
-                rcs[4 * j + i].className="rc color-0";
-                rcs[4 * j + i].innerHTML = "";
 
-            }
         }
 }
 
-document.onkeydown = function (e) {
-    var e = window.event || e;
-    if (e.keyCode === 39)
-    {toRight();mark.innerHTML="<b id='fenshu'>分数</b><br><b id='ans'>"+ans+"</b>";}
-    if (e.keyCode === 37)
-    {toLeft();mark.innerHTML="<b id='fenshu'>分数</b><br><b id='ans'>"+ans+"</b>";}
-    if (e.keyCode === 40)
-    {toDown();mark.innerHTML="<b id='fenshu'>分数</b><br><b id='ans'>"+ans+"</b>";}
-    if (e.keyCode === 38)
-    {toUp();mark.innerHTML="<b id='fenshu'>分数</b><br><b id='ans'>"+ans+"</b>";}
+document.onkeydown = function (ev) {
+    let e = window.event || ev;
+    switch (e.keyCode) {
+        case 39:toRight();break;
+        case 37:toLeft();break;
+        case 40:toDown();break;
+        case 38:toUp();break;
+    }
+    mark.innerHTML=ans;
     renderBoard();
     randomGeneration();
+};
+
+tryagain.onclick=function () {
+    restart();
+
+};
+
+function lose_f() {
+    document.getElementById("final-score").innerHTML=ans;
+    let gameover=document.getElementById("gameover-div");
+    gameover.style.visibility="initial";
+    let total=document.getElementById("total");
+    total.style.opacity="0.4";
+
 }
-function lose_f(){
-    alert("lose");
-    var ans=0;
-    for(var i=0;i<4;i++)
-        for(var j=0;j<4;j++)
-            ans+=board[i][j];
-        str="<p id='gameover'>Gameover</p><b id='ans'>"+ans+"</b>";
-    document.getElementById("lose-interface").innerHTML=str;
+
+
+
+function is_Lose() {
+    for(let i=0;i<4;i++)
+        for(let j=0;j<3;j++)if(board[i][j]===board[i][j+1])
+            return false;
+    for(let i=0;i<3;i++)
+        for(let j=0;j<4;j++)if(board[i][j]===board[i+1][j])
+            return false;
+    return true;
 }
+
 function restart() {
     init();
     renderBoard();
     randomGeneration();
+    let total=document.getElementById("total");
+    total.style.opacity=1;
+    let gameover=document.getElementById("gameover-div");
+    gameover.style.visibility="hidden";
+    let ans=document.getElementById("ans");
+    ans.innerHTML="0";
 }
